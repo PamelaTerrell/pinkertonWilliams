4df672bfe71856738377a928d04e22cdb09f84ff
PamelaTerrell <pammyhoney@yahoo.com>
2025-06-14 15:18:07 -0400

    nav png

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import davLogo from '../assets/davclear.png';  // import the image here
 import './NavBar.css';
 
 export default function NavBar() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          <img src="/src/assets/davclear.png" alt="DAV Chapter 18" />
-        </Link>
-        
-        <button
-          className="menu-toggle"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Toggle Menu"
-        >
-          &#9776;
-        </button>
-
-        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-          <li><Link to="/" className="nav-link">Home</Link></li>
-          <li><Link to="/officers" className="nav-link">Officers</Link></li>
-          <li><Link to="/events" className="nav-link">Events</Link></li>
-        </ul>
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={davLogo} alt="DAV Chapter 18" className="nav-logo" />
+        </Link>   
       </div>
+      <ul className="nav-links">
+        <li><Link to="/" className="nav-link">Home</Link></li>
+        <li><Link to="/officers" className="nav-link">Officers</Link></li>
+        <li><Link to="/events" className="nav-link">Events</Link></li>
+      </ul>
     </nav>
   );
 }
