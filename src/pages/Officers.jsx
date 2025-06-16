509f9932dac7736a7c0f6b515798796ec851a0f9
PamelaTerrell <pammyhoney@yahoo.com>
2025-06-16 16:25:07 -0400

    resizing more

--- src/pages/Officers.jsx
+++ src/pages/Officers.jsx
@@ -5,7 +5,7 @@ import './Officers.css';
 
 const officers = [
   { id: 1, name: "Alvin Terrell", title: "Commander", photo: "/assets/officers/AT.JPG" },
-  { id: 2, name: "Tracey Robinson", title: "Senior Vice Commander", photo: "/assets/officers/tracyrobinson.jpg" },
+  { id: 2, name: "Tracey Robinson", title: "Senior Vice Commander", photo: "/assets/officers/traceyrobinson.png" },
   { id: 3, name: "Eddie Thomas", title: "1st Junior Vice Commander", photo: "/assets/officers/ethomas.jpg" },
   { id: 4, name: "Chris Walker", title: "2nd Junior Vice Commander", photo: "/assets/officers/ChrisWalker.jpg" },
   { id: 5, name: "Donna Thames", title: "Treasurer", photo: "/assets/officers/DonnaThames.jpg" },
@@ -13,7 +13,7 @@ const officers = [
   { id: 7, name: "Arthur Smith", title: "Chapter Historian", photo: "/assets/officers/ArthurSmith.jpeg" },
   { id: 8, name: "Douglas Holloway", title: "Sgt at Arms", photo: "/assets/officers/DouglasHolloway.jpg" },
   { id: 9, name: "Robert Bledsoe", title: "Staff Judge Advocate", photo: "/assets/officers/RB.JPG" },
-  { id: 10, name: "Pamela Terrell", title: "Web Developer", photo: "/assets/officers/PT.JPG" },
+  { id: 10, name: "Pamela Terrell", title: "Web Developer", photo: "/assets/officers/PJT.png" },
 ];
 
 export default function Officers() {
