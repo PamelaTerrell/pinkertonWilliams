7ccc60ffcf0eaa670c0848d71f6d291e955a4613
Pamela Terrell <pammyhoney@yahoo.com>
2026-05-09 10:16:29 -0400

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { Analytics } from "@vercel/analytics/react"
 
 import Home from './pages/Home.jsx'
 import Officers from './pages/Officers.jsx'
 import Events from './pages/Events.jsx'
 import PurpleHeartHonoree from './pages/PurpleHeart.jsx'
-import Membership from './pages/Membership.jsx';
-import ChristmasPartyGallery from './pages/ChristmasPartyGallery.jsx';
-
+import Membership from './pages/Membership.jsx'
+import ChristmasPartyGallery from './pages/ChristmasPartyGallery.jsx'
 
 import NavBar from './components/NavBar.jsx'
 import Footer from './components/Footer.jsx'
 
 import usePageView from './hooks/usePageView.js'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
-  usePageView(); // 
+  usePageView()
 
   return (
     <>
-      <NavBar />
+      <ScrollToTop />
 
-     
+      <NavBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -34,7 +43,8 @@ function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
-      <Footer /> 
+      <Footer />
+
       <Analytics />
     </>
   )
